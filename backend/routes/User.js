e57ecff1a9a37a70e23c30b6987416eb096d0c2d
rishav-sharma-vst-au4 <54503782+rishav-sharma-vst-au4@users.noreply.github.com>
2020-11-22 02:23:10 +0530

    sign up v 1.0 backend

---
+++ backend/routes/User.js
@@ -0,0 +1,46 @@
+const express = require('express')
+const router = express.Router()
+const mongoose = require('mongoose')
+const User = mongoose.model("User")
+const bcrypt = require('bcryptjs')
+
+
+router.post('/signup',(req,res)=>{
+    const {name,email,password,pic} = req.body 
+    if(!email || !password || !name){
+       return res.status(422).json({error:"please add all the fields"})
+    }
+    User.findOne({email:email})
+    .then((savedUser)=>{
+        if(savedUser){
+          return res.status(422).json({error:"user already exists with that email"})
+        }
+        bcrypt.hash(password,12)
+        .then(hashedpassword=>{
+              const user = new User({
+                  email,
+                  password:hashedpassword,
+                  name,
+                  pic
+              })
+      
+              user.save()
+              .then(user=>{
+                res.json({message:"saved successfully"})
+              })
+              .catch(err=>{
+                  console.log(err)
+              })
+        })
+       
+    })
+    .catch(err=>{
+      console.log(err)
+    })
+  })
+
+
+
+
+
+module.exports = router
--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,32 @@
+require("dotenv").config();
 const express = require("express");
+const mongoose = require('mongoose');
+
+
+//mongo
+mongoose.connect(process.env.MONGOURI,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+
+mongoose.connection.on('connected',() =>{
+    console.log("Hey Your db connected so dont worry only you application enjoy ")
+})
+
+mongoose.connection.on('error',(err)=>{
+    console.log("error while connecting db please check me")
+})
 
 //express app
 const app = express()
 app.use(express.json());
 
+//require
+require('./models/user')
 
-app.get('/',(req,res) =>console.log("rishav"));
+//appuse
+app.use(express.json())
+app.use(require('./routes/user'))
 
 //port
 const PORT = process.env.PORT || 1100;
